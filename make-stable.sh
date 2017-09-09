e20e78b66bb4f653ae734e343510ed96cef08a64
Thomas Meyer <thomas@m3y3r.de>
2017-09-09 11:49:51 +0200

    Some fixes

--- make-stable.sh
+++ make-stable.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-VER=4.8
-SRCDIR=~/git/linux-$VER.y
-git worktree add $SRCDIR/ v$VER.17
+VER=4.12
+LINUX_DIR=~/git/linux
+STABLE_DIR=~/git/linux-$VER.y
+
+if [ ! -d "$STABLE_DIR" ]; then
+	mkdir -p $STABLE_DIR
+fi
+
+git -C $LINUX_DIR worktree add $STABLE_DIR/ v$VER.11
 
 export ARCH=x86
-make -j$(nproc) -C $SRCDIR all
+make -j$(nproc) -C $STABLE_DIR all
--- make-x86.sh
+++ make-x86.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+LINUX_DIR=~/git/linux
+
 export ARCH=x86
-export KBUILD_OUTPUT=/home/thomas/git/linux-x86/
-make -j$(nproc) all
+export KBUILD_OUTPUT=~/git/linux-x86/
+make -C $LINUX_DIR -j$(nproc) all
